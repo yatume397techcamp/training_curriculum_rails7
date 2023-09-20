f6ed61590f157c5c3a98772c26d06f1141dc8839
yatume397techcamp <yatume397techcamp@gmail.com>
2023-09-20 14:46:28 +0900

    曜日修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,7 +34,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      wday_num = @todays_date.wday # wdayメソッドを用いて取得した数値
+      wday_num = (@todays_date + x).wday # wdayメソッドを用いて取得した数値
       if wday_num >= 7 #「wday_numが7以上の場合」という条件式
         wday_num = wday_num -7
       end
